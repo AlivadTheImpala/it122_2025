5414d81379e834bc0c2585c5586b2725de984205
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2025-01-11 12:41:49 -0800

    added comments to index.js of current understanding of the concepts practiced in this assigment

--- week1/index.js
+++ week1/index.js
@@ -25,3 +25,13 @@ server.listen(port, () =>
     `server has started on port ${port}. http://localhost:${port}. Press Ctrl-C to terminate the server.`
   )
 );
+
+//a listener is added to the server telling it to listen for activity on port 3000. meanwhile http.createServer creates a new http server, it takes a function with two parameters as its argument. req contains details like the url, http method, headers. while res is used to send data back to the client (ie your browser page).
+//
+// We created the server locally and gave a port to listen to. req.url then, holds the part of the url following the host name and port number.
+//
+//  http://localhost:3000/about becomes /about
+
+//im still unfamiliar with headers, but writeHead() contains a manually set status code for the particular routes and a description of the type of content being sent to the client.
+
+//end() indicates that no more data will be written or sent and the response ends with the respective string in the switches.
