469187811bdc1371bff15294d6c6edb6f1dd0028
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2025-01-16 22:06:13 -0800

    added data.js

---
+++ week2/data.js
@@ -0,0 +1,32 @@
+let albums = [
+  {
+    albumTitle: "Issues",
+    artist: "Korn",
+    released: "November 16, 1999",
+    genre: "Nu Metal",
+  },
+  {
+    albumTitle: "Issues",
+    artist: "Korn",
+    released: "November 16, 1999",
+    genre: "Nu Metal",
+  },
+  {
+    albumTitle: "Issues",
+    artist: "Korn",
+    released: "November 16, 1999",
+    genre: "Nu Metal",
+  },
+  {
+    albumTitle: "Issues",
+    artist: "Korn",
+    released: "November 16, 1999",
+    genre: "Nu Metal",
+  },
+  {
+    albumTitle: "Issues",
+    artist: "Korn",
+    released: "November 16, 1999",
+    genre: "Nu Metal",
+  },
+];
