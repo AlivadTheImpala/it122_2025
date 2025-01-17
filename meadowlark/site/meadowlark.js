143dd71f4d75d91ee989250239b1f28fdd418714
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2025-01-16 22:04:57 -0800

    created meadowlark directory, installed handlebars templater. created week2 directory added data.js

--- meadowlark/site/meadowlark.js
+++ meadowlark/site/meadowlark.js
@@ -1,22 +1,38 @@
 const express = require("express");
-
+const { engine } = require("express-handlebars"); // Destructure the engine function
 const app = express();
+
+app.engine(
+  "handlebars",
+  engine({
+    defaultLayout: "main",
+  })
+); //located in views/layouts
+app.set("view engine", "handlebars");
 
 const port = process.env.PORT || 3000;
 
+// configure Handlebars view engine
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
 // custom 404 page
 app.use((req, res) => {
-  res.type("text/plain");
   res.status(404);
-  res.send("404 - Not Found");
+  res.render("404");
 });
 
 // custom 500 page
 app.use((err, req, res, next) => {
   console.error(err.message);
-  res.type("text/plain");
   res.status(500);
-  res.send("500 - Server Error");
+  res.render("500");
 });
 
 app.listen(port, () =>
