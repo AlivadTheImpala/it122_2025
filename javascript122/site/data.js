1c4b8e9484b2f5d769bc94771c695b2e74a4428d
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2025-01-18 19:44:59 -0800

    new node project initialized
    express and ejs installed, data.js updated with getAll

--- javascript122/site/data.js
+++ javascript122/site/data.js
@@ -4,29 +4,40 @@ let albums = [
     artist: "Korn",
     released: "November 16, 1999",
     genre: "Nu Metal",
+    id: 1,
   },
   {
     albumTitle: "Horizons",
     artist: "Parkway Drive",
     released: "October 6, 2007",
     genre: "Metalcore",
+    id: 2,
   },
   {
     albumTitle: "Exhibition of Prowess",
     artist: "Kublai Khan TX",
     released: "September 20, 2024",
     genre: "Beatdown Hardcore",
+    id: 3,
   },
   {
     albumTitle: "Introspection",
     artist: "Luiz Bonfá",
     released: "October 1, 1972",
     genre: "Bossa Nova",
+    id: 4,
   },
   {
     albumTitle: "Classical Current",
     artist: "Laurindo Almeida",
     released: "September 15, 1969",
     genre: "Jazz",
+    id: 5,
   },
 ];
+
+const getAll = () => {
+  return albums;
+};
+
+export { getAll };
